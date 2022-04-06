29e84c919d21dc3e2e3aed9110ba462f1e195089
FENGMIAO <13631030360@qq.com>
2022-04-07 00:33:01 +0800

    sizeofTest修改

--- SizeOfTest/Program.cs
+++ SizeOfTest/Program.cs
@@ -5,8 +5,11 @@
         static void Main(string[] args)
         {
             Console.WriteLine("int类型的存储空间大小：{0}", sizeof(int));
+            Console.WriteLine("long类型的存储空间大小：{0}", sizeof(long));
+            Console.WriteLine("float类型的存储空间大小：{0}", sizeof(float));
+            Console.WriteLine("double类型的存储空间大小：{0}", sizeof(double));
+            Console.WriteLine("decimal类型的存储空间大小：{0}", sizeof(decimal));
             Console.ReadKey();
         }
     }
 }
-
