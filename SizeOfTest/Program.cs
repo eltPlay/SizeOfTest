b475657b98656bbb3e136241261c3e258af3c2f0
FENGMIAO <13631030360@qq.com>
2022-04-07 00:26:36 +0800

    Initial commit

---
+++ SizeOfTest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SizeOfTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("int类型的存储空间大小：{0}", sizeof(int));
+            Console.ReadKey();
+        }
+    }
+}
+
